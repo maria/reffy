4a9290843d9d3f1485e8b376d8b5c627cfdcc311
marianitadn <maria.nita.dn@gmail.com>
2012-10-26 12:12:38 +0300

    First commit to Github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamX::Application.config.secret_token = '5174ee650f87204e76a744f4141af6c446df204c2000cb1c515988f494ffcb5e95ad85d03c1433ebd1a401cedff507bde95861a26036d0109c4aaa65161c9e9d'
