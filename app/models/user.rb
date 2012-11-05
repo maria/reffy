4836feff06ed2a987996cdda930a7bd816f00910
marianitadn <maria.nita.dn@gmail.com>
2012-11-05 19:22:53 +0200

    Added fb_id to accessible attr of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :city, :email, :initial, :name
+  attr_accessible :city, :initial, :name, :fb_id
 
   has_many :sports, foreign_key: 'sport_id'
   has_many :teams, foreign_key: 'captain_id'
+
 end
