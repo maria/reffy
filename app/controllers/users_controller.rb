d0860019ba5e96a47b97fbf2d694b95c44bc5e50
marianitadn <maria.nita.dn@gmail.com>
2012-11-05 20:24:34 +0200

    commented some

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -104,10 +104,10 @@ end
 
       respond_to do |format|
         if @user.save
-          format.html { :notice => "User was successfully created." }
+       #   format.html { :notice => "User was successfully created." }
           format.json { render :json => @user, :status => :created, :location => @user }
         else
-          format.html { render :action => "new" }
+        #  format.html { render :action => "new" }
           format.json { render :json => @user.errors, :status => :unprocessable_entity }
         end
       end
